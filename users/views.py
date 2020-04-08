66e685d43de453aec0f1cc56ee7d6fe13c60ada8
DoniDev <abduvokhidovdoniyor4@gmail.com>
2020-04-08 16:43:13 +0500

    Configured views

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}')
-            return redirect('home')
+            return redirect('login')
         else:
             messages.warning(request,'Two passwords didn\'t match')
 
